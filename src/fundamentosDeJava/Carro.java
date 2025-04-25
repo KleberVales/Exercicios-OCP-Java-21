7aba333046ab01ee30ae9679e652b416258efd1f
Kleber Vales <ksvales@outlook.pt>
2025-04-25 08:25:45 -0300

    Atualizaçao da classe

--- src/fundamentosDeJava/Carro.java
+++ src/fundamentosDeJava/Carro.java
@@ -1,4 +1,11 @@
 package fundamentosDeJava;
 
 public class Carro {
+    String modelo;
+    int ano;
+
+    void exibirInfo() {
+        System.out.println("Modelo: " + modelo + ", Ano: " + ano);
+    }
+
 }
