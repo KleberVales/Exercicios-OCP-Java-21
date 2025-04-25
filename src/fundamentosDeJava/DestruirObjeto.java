be3f15374a40f066d967fd7153a5de1468d60b4f
Kleber Vales <ksvales@outlook.pt>
2025-04-25 09:27:51 -0300

    terminando exercicio 3

--- src/fundamentosDeJava/DestruirObjeto.java
+++ src/fundamentosDeJava/DestruirObjeto.java
@@ -8,10 +8,28 @@ class Pessoa {
         System.out.println("Pessoa " + nome + " criada.");
     }
 
-    protected void finalize() {
+    @Override
+    protected void finalize() throws Throwable {
         System.out.println("Pessoa " + nome + " destruída.");
     }
 }
 
 public class DestruirObjeto {
+
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("João");
+
+        // Tornando o objeto elegível para o garbage collector
+        p1 = null;
+
+        // Solicitando a coleta de lixo
+        System.gc();
+
+        // Aguardando um pouco para dar tempo do GC agir
+        try {
+            Thread.sleep(1000); // Espera 1 segundo
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
