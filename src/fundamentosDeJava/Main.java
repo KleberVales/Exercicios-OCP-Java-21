8c858285ba56ebd9fc67899ff80fa4c08116609f
Kleber Vales <ksvales@outlook.pt>
2025-04-25 08:28:14 -0300

    Resoluçao do exercicio 1 Fundamentos de Java

---
+++ src/fundamentosDeJava/Main.java
@@ -0,0 +1,28 @@
+package fundamentosDeJava;
+
+class Carro {
+    String modelo;
+    int ano;
+
+    void exibirInfo() {
+        System.out.println("Modelo: " + modelo + ", Ano: " + ano);
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        // Criando o primeiro objeto
+        Carro carro1 = new Carro();
+        carro1.modelo = "Civic";
+        carro1.ano = 2020;
+
+        // Criando o segundo objeto
+        Carro carro2 = new Carro();
+        carro2.modelo = "Corolla";
+        carro2.ano = 2022;
+
+        // Exibindo informações dos carros
+        carro1.exibirInfo();
+        carro2.exibirInfo();
+    }
+}
