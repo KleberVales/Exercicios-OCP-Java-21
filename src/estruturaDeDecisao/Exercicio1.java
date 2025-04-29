be9dc5be79e1ef99aa67e193599c74255b6c9738
Kleber Vales <ksvales@outlook.pt>
2025-04-29 09:53:51 -0300

    Exercicio numero 1

---
+++ src/estruturaDeDecisao/Exercicio1.java
@@ -0,0 +1,23 @@
+package estruturaDeDecisao;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = sc.nextInt();
+
+        if (numero > 0) {
+            System.out.println("O número é positivo.");
+        } else if (numero < 0) {
+            System.out.println("O número é negativo.");
+        } else {
+            System.out.println("O número é zero.");
+        }
+
+        sc.close();
+    }
+}
+
