943db9458586f270da8f13432f60172c3863165c
Kleber Vales <ksvales@outlook.pt>
2025-05-07 22:18:22 -0300

    exercicio 1

--- src/colecoes/ListaDeNomes.java
+++ src/colecoes/ListaDeNomes.java
@@ -1,4 +1,32 @@
 package colecoes;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class ListaDeNomes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> nomes = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Digite um nome: ");
+            nomes.add(scanner.nextLine());
+        }
+
+        System.out.println("\nLista original:");
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+
+        Collections.sort(nomes);
+
+        System.out.println("\nLista ordenada:");
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+
+        scanner.close();
+    }
 }
+
